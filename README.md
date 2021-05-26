c2156e3ef31f6ef6974515f7285683fc0a632a79
Mauro Strione <mauro.strione@gmail.com>
2021-05-26 01:52:17 -0400

    Styles customizations and translation

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# Agile Battleship
+# Agile Battleship V1.0
 
-A simple battleship game to show the power of direct feedback in Scrum
+A simple battleship game to show the power of short feedback loops in incertain context.
 
 Simply execute index.html and play!
+
+Modified by Mauro Strione
 
 Credits to Zilverline / Daniël van Hoesel, Bob Forma, Mark Suurmond, inspired by  [battleship](https://github.com/zilverline/battleship)
--- javascripts/app/Game.js
+++ javascripts/app/Game.js
@@ -120,15 +120,15 @@ var GameView = Backbone.View.extend({
     if (endGameState === "lose") {
       console.log(diff);
       if(diff > 0){
-        $("#endGameResult").html('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-flag-checkered"></i> ¡Juego Finalizado!</strong> Ganaste ' + this.euros(diff) + '</div>');
+        $("#endGameResult").html('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="bi bi-emoji-smile"></i> ¡Game over!</strong> You made a profit of ' + this.euros(diff) + '</div>');
       }
       else if(diff < 0){
-        $("#endGameResult").html('<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-flag-checkered"></i> ¡Juego Finalizado!</strong> Perdiste ' + this.euros(diff) + '</div>');
+        $("#endGameResult").html('<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="bi bi-emoji-frown"></i> ¡Game over!</strong> You lost ' + this.euros(diff) + '</div>');
       } else {
-        $("#endGameResult").html('<div class="alert alert-info alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-flag-checkered"></i> ¡Juego Finalizado!</strong> Mantuviste tu dinero</div>');
+        $("#endGameResult").html('<div class="alert alert-info alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="bi bi-emoji-neutral"></i> ¡Game over!</strong> You ended up with no profit or loss</div>');
       }
     } else {
-      $("#endGameResult").html('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-trophy"></i> ¡Has destruido la flota!</strong> Ganaste ' + this.euros(diff) + '</div>');
+      $("#endGameResult").html('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="bi bi-emoji-sunglasses"></i> ¡You have destroyed the entire fleet!</strong> You rock...' + this.euros(diff) + '</div>');
     }
   },
   euros: function(amount) {
